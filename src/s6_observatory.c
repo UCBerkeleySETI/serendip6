da931e0ee450d6ec5b9bee2df7dd5f1bf6f30710
Jeff Cobb <jeffc@paper2.paper.pvt>
2014-02-02 15:50:58 -0800

    Added the observatory code.

---
+++ src/s6_observatory.c
@@ -0,0 +1,241 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include <malloc.h>
+#include <string.h>
+#include <pthread.h>
+#include <fmtmsg.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/timeb.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <ctype.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+//extern "C"
+//{
+#include "mshmLib.h"
+#include "execshm.h"
+#include "wappshm.h"
+#include "wapplib.h"
+#include "alfashm.h"
+#include "vtxLib.h"
+#include "scram.h"
+
+#include <hiredis.h>
+
+//}
+
+#include "s6obsaux.h"
+
+// #define bool int
+// #define true  1
+// #define false 0
+// static const double D2R=(M_PI/180.0);
+
+int main(int argc, char ** argv) {
+    struct SCRAMNET * scram;
+    char    name[256];
+    int     i;
+    bool    got_pnt, got_agc, got_alfashm;
+    time_t  time_pnt, time_agc, time_if1, time_if2, time_tt, time_alfashm, time_fix;
+    double Enc2Deg = 1./ ( (4096. * 210. / 5.0) / (360.) );
+
+    char strbuf[1024];
+    redisContext *c;
+    redisReply *reply;
+    const char *hostname = "127.0.0.1";
+    int port = 6379;
+
+    const char *usage = "Usage: s6_observatory [-test] [-stdout] [-nodb] [-nottl] [-hostname hostname] [-port port]\n  -test: don't read scram, put in dummy values\n  -stdout: output packets to stdout (normally quiet)\n  -nodb: don't update redis db\n  -nottl: don't expire any of the scram keys in the redis db\n  hostname/port: for redis database (default 127.0.0.1:6379)\n\n";
+
+    // int x = 0; // for debugging
+
+    // fprintf(stderr, "scram size : %d   Enc2Deg = %20.10lf  an int is %d bytes long\n", sizeof(scram), Enc2Deg, sizeof(int));
+
+    bool dotest = false;
+    bool dostdout = false;
+    bool nodb = false;
+    bool nottl = false;
+
+    double RA, Dec, MJD, azfix, zafix; // PNT vars
+    int mlasttck, Az, ZA, agctime; double Azdeg, ZAdeg, timesecs, Azerrdeg, ZAerrdeg; // AGC vars
+    int synIDB_0, fltrbank; double synIHz_0, rfFreq, FrqMhz; // IF1 vars
+    bool useAlfa; // IF2 vars
+    int encoder; double degrees; // TT vars
+    int fstbias, sndbias; double motorpos; // ALFASHM vars
+
+    double beamAz, beamZA; // ra/dec conversion per beam vars
+    double coord_unixtime;
+    double fixedRA[7]; // for fixed values
+    double fixedDec[7];
+
+    for (i = 1; i < argc; i++) {
+      if (strcmp(argv[i],"-test") == 0) { dotest = true; }
+      else if (strcmp(argv[i],"-stdout") == 0) { dostdout = true; }
+      else if (strcmp(argv[i],"-nodb") == 0) { nodb = true; }
+      else if (strcmp(argv[i],"-nottl") == 0) { nottl = true; }
+      else if (strcmp(argv[i],"-hostname") == 0) { hostname = argv[++i]; }
+      else if (strcmp(argv[i],"-port") == 0) { port = atoi(argv[++i]); }
+      else { fprintf(stderr,"%s",usage); exit (1); }
+      }
+
+    if (!nodb) {
+      struct timeval timeout = { 1, 500000 }; // 1.5 seconds
+      c = redisConnectWithTimeout(hostname, port, timeout);
+      if (c == NULL || c->err) {
+          if (c) {
+              printf("Connection error: %s\n", c->errstr);
+              redisFree(c);
+            } else {
+              printf("Connection error: can't allocate redis context\n");
+            }
+          exit(1);
+        }
+      }
+
+    if (!dotest) scram = init_scramread(NULL);
+
+    got_pnt = got_agc = got_alfashm = false;
+    time_pnt = time_agc = time_if1 = time_if2 = time_tt = time_alfashm = time_fix = 0;
+
+    while (1) {
+
+      if (!dotest) {
+
+        if (read_scram(scram) == -1) {
+            fprintf(stderr, "GetScramData : bad scram read\n");
+            exit(1);
+          }
+
+        getnameinfo((const struct sockaddr *)&scram->from, sizeof(struct sockaddr_in), name, 256, NULL, 0, 0);
+
+        if (strcmp(scram->in.magic, "PNT") == 0) {
+            got_pnt = true;
+            time_pnt = time(NULL);
+            RA  = scram->pntData.st.x.pl.curP.raJ;
+            Dec = scram->pntData.st.x.pl.curP.decJ;
+            RA  *= 24.0 / C_2PI;
+            Dec *= 360.0 / C_2PI;
+            MJD  = scram->pntData.st.x.pl.tm.mjd + scram->pntData.st.x.pl.tm.ut1Frac;
+            azfix = scram->pntData.st.x.modelCorEncAzZa[0];
+            zafix = scram->pntData.st.x.modelCorEncAzZa[1];
+            sprintf(strbuf,"SCRAM:PNT PNTSTIME %ld PNTRA %0.10lf PNTDEC %0.10lf PNTMJD %0.10lf PNTAZCOR %0.10lf PNTZACOR %0.10lf",time_pnt,RA,Dec,MJD,azfix,zafix);
+            if (!nodb) { reply = redisCommand(c,"HMSET %s",strbuf); freeReplyObject(reply); }
+            if (dostdout) fprintf(stderr,"%s\n",strbuf);
+          } else if (strcmp(scram->in.magic, "AGC") == 0) {
+            got_agc = true;
+            time_agc = time(NULL);
+            mlasttck = scram->agcData.st.secMLastTick;
+            Az = scram->agcData.st.cblkMCur.dat.posAz;
+            Azdeg = scram->agcData.st.cblkMCur.dat.posAz * 0.0001;
+            ZA = scram->agcData.st.cblkMCur.dat.posGr; 
+            ZAdeg = scram->agcData.st.cblkMCur.dat.posGr * 0.0001;
+            agctime = scram->agcData.st.cblkMCur.dat.timeMs;
+            timesecs = scram->agcData.st.cblkMCur.dat.timeMs * 0.001; 
+            Azerrdeg = scram->agcData.st.posErr.reqPosDifRd[0];
+            ZAerrdeg = scram->agcData.st.posErr.reqPosDifRd[1];
+            // WHAT IS AGCLST?!?!?!! TODO
+            sprintf(strbuf,"SCRAM:AGC AGCSTIME %ld AGCTIME %d AGCAZ %0.10lf AGCZA %0.10lf AGCLST %0.10lf",time_agc,agctime,Azdeg,ZAdeg,0.0);
+            if (!nodb) { reply = redisCommand(c,"HMSET %s",strbuf); freeReplyObject(reply); }
+            if (dostdout) fprintf(stderr,"%s\n",strbuf);
+          } else if (strcmp(scram->in.magic, "IF1") == 0) {
+            time_if1 = time(NULL); 
+            synIHz_0 = scram->if1Data.st.synI.freqHz[0];
+            synIDB_0 = scram->if1Data.st.synI.ampDb[0];
+            rfFreq = scram->if1Data.st.rfFreq;
+            FrqMhz = scram->if1Data.st.if1FrqMhz;
+            fltrbank = scram->if1Data.st.stat2.alfaFb;
+            sprintf(strbuf,"SCRAM:IF1 IF1STIME %ld IF1SYNHZ %0.10lf IF1SYNDB %d IF1RFFRQ %0.10lf IF1IFFRQ %0.10lf IF1ALFFB %d",time_if1,synIHz_0,synIDB_0,rfFreq,FrqMhz,fltrbank);
+            if (!nodb) { reply = redisCommand(c,"HMSET %s",strbuf); freeReplyObject(reply); }
+            if (dostdout) fprintf(stderr,"%s\n",strbuf);
+          } else if (strcmp(scram->in.magic, "IF2") == 0) {
+            time_if2 = time(NULL); 
+            if(scram->if2Data.st.stat1.useAlfa) { useAlfa = true; } else { useAlfa = false; }
+            sprintf(strbuf,"SCRAM:IF2 IF2STIME %ld IF2ALFON %d",time_if2,useAlfa);
+            if (!nodb) { reply = redisCommand(c,"HMSET %s",strbuf); freeReplyObject(reply); }
+            if (dostdout) fprintf(stderr,"%s\n",strbuf);
+          } else if (strcmp(scram->in.magic, "TT") == 0) {
+            time_tt = time(NULL);
+            encoder = scram->ttData.st.slv[0].inpMsg.position;
+            degrees = (double)(scram->ttData.st.slv[0].inpMsg.position) * Enc2Deg;
+            sprintf(strbuf,"SCRAM:TT TTSTIME %ld TTTURENC %d TTTURDEG %0.10lf",time_tt,encoder,degrees);
+            if (!nodb) { reply = redisCommand(c,"HMSET %s",strbuf); freeReplyObject(reply); }
+            if (dostdout) fprintf(stderr,"%s\n",strbuf);
+          } else if (strcmp(scram->in.magic, "ALFASHM") == 0) {
+            got_alfashm = true; 
+            time_alfashm = time(NULL);
+            fstbias = (int)scram->alfa.first_bias;
+            sndbias = (int)scram->alfa.second_bias;
+            motorpos = scram->alfa.motor_position; 
+            sprintf(strbuf,"SCRAM:ALFASHM ALFSTIME %ld ALFBIAS1 %d ALFBIAS2 %d ALFMOPOS %0.10lf",time_alfashm,fstbias,sndbias,motorpos);
+            if (!nodb) { reply = redisCommand(c,"HMSET %s",strbuf); freeReplyObject(reply); }
+            if (dostdout) fprintf(stderr,"%s\n",strbuf);
+          } else {
+            // fprintf(stderr, "UNKNOWN SCRAM: %ld %s from %s\n", current, scram->in.magic, name);
+          }
+
+        if (got_alfashm && got_agc && got_pnt) {
+          time_fix = time_alfashm;
+          if (time_agc > time_fix) time_fix = time_agc;
+          if (time_pnt > time_fix) time_fix = time_pnt;
+          coord_unixtime = s6_seti_ao_timeMS2unixtime(agctime,time_fix);
+          fprintf(stderr,"agctime %d time_fix %ld coord_unixtime %lf\n",agctime,time_fix,coord_unixtime);
+          for (i=0;i<7;i++) {
+            beamAz = Azdeg; beamZA = ZAdeg; // in degrees
+            fprintf(stderr,"i %d Az %lf ZA %lf ",i,beamAz,beamZA);
+            beamAz -= azfix; beamZA -= zafix; // fix also in degrees
+            fprintf(stderr,"fixed: Az %lf ZA %lf ",beamAz,beamZA);
+            s6_BeamOffset(&beamAz, &beamZA, i, motorpos); // i is beam
+            fprintf(stderr,"post offset: Az %lf ZA %lf \n",beamAz,beamZA);
+            s6_AzZaToRaDec(beamAz, beamZA, coord_unixtime, &fixedRA[i], &fixedDec[i]);
+            }
+          sprintf(strbuf,"SCRAM:DERIVED DERTIME %ld RA0 %0.10lf DEC0 %0.10lf RA1 %0.10lf DEC1 %0.10lf RA2 %0.10lf DEC2 %0.10lf RA3 %0.10lf DEC3 %0.10lf RA4 %0.10lf DEC4 %0.10lf RA5 %0.10lf DEC5 %0.10lf RA6 %0.10lf DEC6 %0.10lf",time_fix,fixedRA[0],fixedDec[0],fixedRA[1],fixedDec[1],fixedRA[2],fixedDec[2],fixedRA[3],fixedDec[3],fixedRA[4],fixedDec[4],fixedRA[5],fixedDec[5],fixedRA[6],fixedDec[6]);
+          if (!nodb) { reply = redisCommand(c,"HMSET %s",strbuf); freeReplyObject(reply); }
+          if (dostdout) fprintf(stderr,"%s\n",strbuf);
+          }
+  
+        } // end if !nodotest
+
+      else { // test mode
+
+        sprintf(strbuf,"SCRAM:PNT PNTSTIME %ld PNTRA 1.1 PNTDEC 2.2 PNTMJD 3.3 PNTAZCOR 4.4 PNTZACOR 5.5",time(NULL));
+        if (!nodb) { reply = redisCommand(c,"HMSET %s",strbuf); freeReplyObject(reply); }
+        if (dostdout) fprintf(stderr,"%s\n",strbuf);
+        sprintf(strbuf,"SCRAM:AGC AGCSTIME %ld AGCTIME 1 AGCAZ 2.2 AGCZA 3.3 AGCLST 4.4",time(NULL));
+        if (!nodb) { reply = redisCommand(c,"HMSET %s",strbuf); freeReplyObject(reply); }
+        if (dostdout) fprintf(stderr,"%s\n",strbuf);
+        sprintf(strbuf,"SCRAM:IF1 IF1STIME %ld IF1SYNHZ 1.1 IF1SYNDB 2 IF1RFFRQ 3.3 IF1IFFRQ 4.4 IF1ALFFB 5",time(NULL));
+        if (!nodb) { reply = redisCommand(c,"HMSET %s",strbuf); freeReplyObject(reply); }
+        if (dostdout) fprintf(stderr,"%s\n",strbuf);
+        sprintf(strbuf,"SCRAM:IF2 IF2STIME %ld IF2ALFON 1",time(NULL));
+        if (!nodb) { reply = redisCommand(c,"HMSET %s",strbuf); freeReplyObject(reply); }
+        if (dostdout) fprintf(stderr,"%s\n",strbuf);
+        sprintf(strbuf,"SCRAM:TT TTSTIME %ld TTTURENC 1 TTTURDEG 2.2",time(NULL));
+        if (!nodb) { reply = redisCommand(c,"HMSET %s",strbuf); freeReplyObject(reply); }
+        if (dostdout) fprintf(stderr,"%s\n",strbuf);
+        sprintf(strbuf,"SCRAM:ALFASHM ALFSTIME %ld ALFBIAS1 1 ALFBIAS2 2 ALFMOPOS 3.3",time(NULL));
+        if (!nodb) { reply = redisCommand(c,"HMSET %s",strbuf); freeReplyObject(reply); }
+        if (dostdout) fprintf(stderr,"%s\n",strbuf);
+        sprintf(strbuf,"SCRAM:DERIVED DERTIME %ld RA0 0.0 DEC0 0.0 RA1 1.0 DEC1 1.0 RA2 2.0 DEC2 2.0 RA3 3.0 DEC3 3.0 RA4 4.0 DEC4 4.0 RA5 5.0 DEC5 5.0 RA6 6.0 DEC6 6.0",time(NULL));
+        if (!nodb) { reply = redisCommand(c,"HMSET %s",strbuf); freeReplyObject(reply); }
+        if (dostdout) fprintf(stderr,"%s\n",strbuf);
+        
+        sleep(1); // just so we make it look more like scram and don't hammer redis db
+
+        } // end if dotest
+
+      } // end while (1)
+
+    exit(0);
+}
---
+++ src/s6obsaux.c
@@ -0,0 +1,149 @@
+#include <stdlib.h>
+#include "s6obsaux.h"
+#include <math.h>
+#include "azzaToRaDec.h"
+
+/// all this code is from setilib, which is in cpp so can't link against it.
+/// NOTE: the call the phils code is set to precess which is DIFFERENT than
+///       the setilib code, which is set to NOT precess.
+
+double s6_seti_ao_timeMS2unixtime(long timeMs, time_t now) {
+//=======================================================
+// converts millisecs past midnight to a unix time_t.
+// Time is often given as millisecs past midnight in AO
+// scramnet structures.
+// The "now" variable needs to be unix time ahead of
+// timeMs by less that 24 hours.  The realtime code can
+// just pass the result of time().  Backend code code
+// typically will use the SysTime included with each scramnet
+// structure.
+
+    static const double AST_TO_UTC_HOURS=4;
+    double secs_past_midnight, encoder_read_secs_past_midnight,fnow;
+    struct tm *now_tm;
+
+    // craft a tm structure with values corresponding to encoder
+    // read time don't use localtime() because it will mess up
+    // when data is read on pacific time computers
+    // by correcting for the PST-UT difference
+    fnow=now;
+    fnow-=AST_TO_UTC_HOURS*3600;                 // translate UTC to AST
+    now=(time_t)floor(fnow);
+    now_tm = gmtime(&now);
+    secs_past_midnight = now_tm->tm_hour*3600 + now_tm->tm_min*60 + now_tm->tm_sec;
+    encoder_read_secs_past_midnight = (timeMs * 0.001);
+    fnow -= secs_past_midnight;              // just back up to midnight
+    fnow += encoder_read_secs_past_midnight;     // now now as the unix time of the encoder reading
+    fnow+=AST_TO_UTC_HOURS*3600;                // translate AST to UTC
+    return(fnow);
+}
+
+bool s6_BeamOffset(double *Az, double *ZA, int Beam, double AlfaMotorPosition) {
+
+  double array_angle[7] = {0.0, 120.0, 180.0, -120.0, -60.0, 0.0, 60.0};
+  double array_az_ellipse;
+  double array_za_ellipse;
+
+  if(Beam != 0) {
+          array_az_ellipse = 329.06;  //taken from seti_boinc/db/tools/s4_receivers.xml
+          array_za_ellipse = 384.005;
+   } else {
+          array_az_ellipse = 0;
+          array_za_ellipse = 0;
+   }
+
+  double AzCorrection = *Az;
+  double ZenCorrection = *ZA;
+
+  double posrot=(array_angle[Beam] - AlfaMotorPosition)*D2R; 
+  AzCorrection+=array_az_ellipse*cos(posrot);
+  ZenCorrection+=array_za_ellipse*sin(posrot);
+
+  *ZA -= ZenCorrection / 3600.0;                 // Correction is in arcsec.
+  *Az -= (AzCorrection  / 3600.0) / sin(*ZA * D2R);
+
+  // Correction is in arcsec.
+
+  // Sometimes the azimuth is not where the telescope is
+  // pointing, but rather where it is physically positioned.
+  *Az    += 180.0; //taken from seti_boinc/db/tools/s4_receivers.xml
+
+  if(*ZA < 0.0)                                // Have we corrected zen over
+    {                                           //  the zenith?
+    *ZA = 0.0 - *ZA;                          // If so, correct zen and
+    *Az += 180.0;                               //   swing azimuth around to
+    }
+
+  *Az = wrap(*Az, 0, 360, 1);              // az to 0 - 360 degrees
+
+  return true;
+}
+
+void s6_AzZaToRaDec(double Az, double Za, double coord_time, double *Ra, double *Dec) {
+//=======================================================
+// This calls AO Phil's code.
+// Any desired model correction must be done prior to calling this routine.
+
+    AZZA_TO_RADEC_INFO  azzaI; /* info for aza to ra dec*/
+
+    const double d2r =  0.017453292;
+    int dayNum, i_mjd;
+    int ofDate = 0;     // if 1, tells azzaToRaDec() to return coords of the date, ie not precessed
+    double utcFrac;
+    struct tm *coord_tm;
+    time_t lcoord_time=(time_t)floor(coord_time);
+    double fcoord_time=coord_time-floor(coord_time);
+
+    coord_tm = gmtime(&lcoord_time);
+
+    // arithmetic needed by AO functions
+    coord_tm->tm_mon += 1;
+    coord_tm->tm_year += 1900;
+    dayNum = dmToDayNo(coord_tm->tm_mday,coord_tm->tm_mon,coord_tm->tm_year);
+    i_mjd=gregToMjd(coord_tm->tm_mday, coord_tm->tm_mon, coord_tm->tm_year);
+    utcFrac=(coord_tm->tm_hour*3600 + coord_tm->tm_min*60 + coord_tm->tm_sec + fcoord_time)/86400.;
+    if (utcFrac >= 1.) {
+        i_mjd++;
+        utcFrac-=1.;
+    }
+
+    // call the AO functions
+    if (azzaToRaDecInit(dayNum, coord_tm->tm_year, &azzaI) == -1) {
+        exit(1);
+    }
+    // subtract 180 from Az because Phil wants dome azimuth
+    azzaToRaDec(Az-180.0, Za, i_mjd, utcFrac, ofDate, &azzaI, Ra, Dec);
+
+    // Ra to hours and Dec to degrees
+    *Ra = (*Ra / d2r) / 15;
+    *Dec = *Dec / d2r;
+
+    // Take care of wrap situations in RA
+    while (*Ra < 0) {
+        *Ra += 24;
+    }
+    *Ra = fmod(*Ra,24);
+}
+
+//__________________________________________________________________________
+double wrap(double Val, long LowBound, long HighBound, bool LowInclusive) {
+//__________________________________________________________________________
+
+    if (LowInclusive) {
+        while (Val >= HighBound) {
+            Val -= HighBound;
+        }
+        while (Val <   LowBound) {
+            Val += HighBound;
+        }
+    } else {
+        while (Val >  HighBound) {
+            Val -= HighBound;
+        }
+        while (Val <=  LowBound) {
+            Val += HighBound;
+        }
+    }
+    return Val;
+}
+
