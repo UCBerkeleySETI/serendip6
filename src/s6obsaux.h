80d131a85fe141b10dbda0e09e23777ab7f4efa7
Jeff Cobb <jeffc@snb1.roach.pvt>
2014-02-12 14:39:58 -0800

    Added s6obsaux.h.

---
+++ src/s6obsaux.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#define bool int
+#define true  1
+#define false 0
+
+static const double D2R=(M_PI/180.0);
+
+double s6_seti_ao_timeMS2unixtime(long timeMs, time_t now);
+bool s6_BeamOffset(double *Az, double *ZA, int Beam, double AlfaMotorPosition);
+void s6_AzZaToRaDec(double Az, double Za, double coord_time, double *Ra, double *Dec);
+double wrap(double Val, long LowBound, long HighBound, bool LowInclusive);
