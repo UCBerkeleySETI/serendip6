feba054df7852b741e9a55ddbfd5c58c5c808965
Jeff Cobb <jeffc@ssl.berkeley.edu>
2014-02-27 09:23:41 -0400

    Removed s6_seti_ao_timeMS2unixtime().
    It had been commneted out and moved to s6_time.c.

--- src/s6_obsaux.c
+++ src/s6_obsaux.c
@@ -7,39 +7,6 @@
 /// NOTE: the call the phils code is set to precess which is DIFFERENT than
 ///       the setilib code, which is set to NOT precess.
 
-#if 0
-double s6_seti_ao_timeMS2unixtime(long timeMs, time_t now) {
-//=======================================================
-// converts millisecs past midnight to a unix time_t.
-// Time is often given as millisecs past midnight in AO
-// scramnet structures.
-// The "now" variable needs to be unix time ahead of
-// timeMs by less that 24 hours.  The realtime code can
-// just pass the result of time().  Backend code code
-// typically will use the SysTime included with each scramnet
-// structure.
-
-    static const double AST_TO_UTC_HOURS=4;
-    double secs_past_midnight, encoder_read_secs_past_midnight,fnow;
-    struct tm *now_tm;
-
-    // craft a tm structure with values corresponding to encoder
-    // read time don't use localtime() because it will mess up
-    // when data is read on pacific time computers
-    // by correcting for the PST-UT difference
-    fnow=now;
-    fnow-=AST_TO_UTC_HOURS*3600;                 // translate UTC to AST
-    now=(time_t)floor(fnow);
-    now_tm = gmtime(&now);
-    secs_past_midnight = now_tm->tm_hour*3600 + now_tm->tm_min*60 + now_tm->tm_sec;
-    encoder_read_secs_past_midnight = (timeMs * 0.001);
-    fnow -= secs_past_midnight;              // just back up to midnight
-    fnow += encoder_read_secs_past_midnight;     // now now as the unix time of the encoder reading
-    fnow+=AST_TO_UTC_HOURS*3600;                // translate AST to UTC
-    return(fnow);
-}
-#endif
-
 bool s6_BeamOffset(double *Az, double *ZA, int Beam, double AlfaMotorPosition) {
 
   double array_angle[7] = {0.0, 120.0, 180.0, -120.0, -60.0, 0.0, 60.0};
