f7b02c7ebb5b61873dd4d1c87745afa344b81456
Matt Lebofsky <mlebofsk@prospero.gb.nrao.edu>
2015-06-01 16:00:24 -0400

    more ui updates to s6obs_gbt.c

--- src/s6obs_gbt.c
+++ src/s6obs_gbt.c
@@ -6,15 +6,22 @@
 
 #include <hiredis.h>
 
+#include "s6obs_gbt_aux.h"
+
+#define SLEEP_MICROSECONDS 100000
+
 // file containing redis key/mysql row pairs for which to read from mysql and put into redis
 const char *status_fields_config = "./status_fields";
+
+const char *usage = "Usage: s6obs_gbt [-stdout] [-nodb] [-hostname hostname] [-port port]\n  -stdout: output packets to stdout (normally quiet)\n  -nodb: don't update redis db\n  hostname/port: for redis database (default 127.0.0.1:6379)\n\n";
 
 MYSQL* mysql;
 MYSQL_ROW row;
 MYSQL_RES* resp;
 int retval;
 
-int main() {
+int main(int argc, char ** argv) {
+
   int i;
 
   redisContext *c;
@@ -28,7 +35,45 @@ int main() {
   FILE *statusfp;
   int numkeys;
 
+  bool nodb = false;
+  bool dostdout = false;
+
+  int az_actual_index, el_actual_index, last_update_index; // make sure we have these
+
+  double az_actual, el_actual, ra_derived, dec_derived;
+
+  char last_last_update[32];
+
+  //### read in command line arguments
+
+  for (i = 1; i < argc; i++) {
+      if (strcmp(argv[i],"-nodb") == 0) {
+          // do not update the redis DB
+          nodb = true;
+      }
+      else if (strcmp(argv[i],"-stdout") == 0) {
+          // output values to stdout.  Can be used with or without
+          // -nodb but is nearly always specified when -nodb is used.
+          dostdout = true;
+      }
+      else if (strcmp(argv[i],"-hostname") == 0) {
+          // the host on which the redis DB resides
+          hostname = argv[++i];
+      }
+      else if (strcmp(argv[i],"-port") == 0) {
+          // the port on which the redis DB server is listening
+          port = atoi(argv[++i]);
+      }
+      else {
+          fprintf(stderr,"%s",usage); exit (1);
+      }
+  }
+
   //### read in status fields (and create mysql "select" command string)
+  
+  az_actual_index = -1; 
+  el_actual_index = -1;
+  last_update_index = -1;
   char *selectcommand = (char *)malloc(sizeof(char*)*lines_allocated*max_line_len);
   char **fitskeys= (char **)malloc(sizeof(char*)*lines_allocated);
   char **mysqlkeys= (char **)malloc(sizeof(char*)*lines_allocated);
@@ -43,8 +88,16 @@ int main() {
       mysqlkeys[i] = malloc(max_line_len);
       if (fitskeys[i]==NULL || mysqlkeys[i]==NULL) { fprintf(stderr,"Out of memory (getting next line).\n"); exit(4); }
       if (fscanf(statusfp,"%s %s",fitskeys[i],mysqlkeys[i])!=2) break;
+      if (strcmp(mysqlkeys[i],"az_actual") == 0) az_actual_index = i;
+      if (strcmp(mysqlkeys[i],"el_actual") == 0) el_actual_index = i;
+      if (strcmp(mysqlkeys[i],"last_update") == 0) last_update_index = i;
       }
   fclose(statusfp);
+
+  if (az_actual_index == -1 || el_actual_index == -1 || last_update_index == -1) {
+    fprintf(stderr,"az_actual, el_actual, last_update must all be represented in status fields file: %s",status_fields_config);
+    exit(5);
+    }
 
   numkeys = i;
 
@@ -61,24 +114,38 @@ int main() {
   // fprintf(stderr,"command: %s\n",selectcommand);
   
   //### connect to redis db
-  struct timeval timeout = { 1, 500000 }; // 1.5 seconds
-  c = redisConnectWithTimeout(hostname, port, timeout);
-  if (c == NULL || c->err) {
-      if (c) {
-          printf("Connection error: %s\n", c->errstr);
-          redisFree(c);
-        } else {
-          printf("Connection error: can't allocate redis context\n");
-        }
-      exit(1);
+
+  if (!nodb) {
+    struct timeval timeout = { 1, 500000 }; // 1.5 seconds
+    c = redisConnectWithTimeout(hostname, port, timeout);
+    if (c == NULL || c->err) {
+        if (c) {
+            printf("Connection error: %s\n", c->errstr);
+            redisFree(c);
+          } else {
+            printf("Connection error: can't allocate redis context\n");
+          }
+        exit(1);
+      }
+    }
+  
+  //### connect to mysql db
+  
+  mysql = mysql_init(0);
+  if (!mysql_real_connect (&mysql, "gbtdata.gbt.nrao.edu","gbtstatus","w3bqu3ry","gbt_status",3306,0,CLIENT_FOUND_ROWS); {
+    fprintf(stderr, "Failed to connect to database: Error: %s\n", mysql_error(&mysql));
+    exit(6);
     }
 
-  //### connect to mysql db
-  mysql = mysql_init(0);
-  mysql = mysql_real_connect (mysql, "gbtdata.gbt.nrao.edu","gbtstatus","w3bqu3ry","gbt_status",3306,0,CLIENT_FOUND_ROWS);
+  //### any other preparations before main loop?
+  
+  strcpy(last_last_update,"");
 
   //### MAIN LOOP
+  
   while (1) {
+
+    // read from mysql database
 
     retval = mysql_query(mysql,selectcommand);
     if (retval) {
@@ -95,23 +162,40 @@ int main() {
       fprintf(stderr,"error fetching mysql row - exiting...\n");
       exit(1);
       }
-    
-    for (i=0;i<numkeys;i++) {
-      // fprintf(stderr,"%d %s\n",i,row[i]);
-      // sprintf(strbuf,"ROW%d \"%s\"",i,row[i]);
-      // fprintf(stderr,"%s\n",strbuf);
-      // reply = redisCommand(c,"SET %s",strbuf);
-      reply = redisCommand(c,"SET %s \"%s\"",fitskeys[i],row[i]);
-      fprintf(stderr, "SET: %s\n", reply->str);
 
-      freeReplyObject(reply); 
+    // calculations
+
+    if (strcmp(last_last_update,row[last_update_index]) == 0) {
+      // fprintf(stderr,"not updated.. skipping..\n");
+      usleep(SLEEP_MICROSECONDS); // sleep one half a second (enough to ensure update within +/- 1 second)
+      continue;  
+      }
+    strcpy(last_last_update,row[last_update_index]);
+    
+    // update redis db (if so desired)
+
+    if (!nodb) {
+      for (i=0;i<numkeys;i++) {
+        // fprintf(stderr,"%d %s\n",i,row[i]);
+        // sprintf(strbuf,"ROW%d \"%s\"",i,row[i]);
+        // fprintf(stderr,"%s\n",strbuf);
+        // reply = redisCommand(c,"SET %s",strbuf);
+        reply = redisCommand(c,"SET %s \"%s\"",fitskeys[i],row[i]);
+        // fprintf(stderr, "SET: %s\n", reply->str);
+        freeReplyObject(reply); 
+        }
+      }
+ 
+    //display values to stdout (if so desired)
+     
+    if (dostdout) {
+      for (i=0;i<numkeys;i++) printf("%2d %8s (%32s) : %s\n",i,fitskeys[i],mysqlkeys[i],row[i]);
+      printf("-----------\n");
       }
 
-    fprintf(stderr,"------\n");
-    
     mysql_free_result(resp);
 
-    sleep(1);
+    usleep(SLEEP_MICROSECONDS); // sleep one half a second (enough to ensure update within +/- 1 second)
   
     } // end main while loop
   
