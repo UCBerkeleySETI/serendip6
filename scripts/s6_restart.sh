ff6b2e6f8196d0836030dfd98d85a13a5d661725
Jeff Cobb <jeffc@ssl.berkeley.edu>
2016-06-24 18:51:36 -0400

    Script mods due to operational changes.

--- scripts/s6_restart.sh
+++ scripts/s6_restart.sh
@@ -6,3 +6,7 @@ export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/cuda/lib64:/opt/intel/lib/intel
 cd /data/serendip6 ; pkill -f "hashpipe -p serendip6" ; /usr/local/bin/s6_init.sh s6_pktsock_thread
 hashpipe_check_status -k RUNALWYS -I 0 -s 1
 hashpipe_check_status -k RUNALWYS -I 1 -s 1
+hashpipe_check_status -k IDLE     -I 0 -s 0
+hashpipe_check_status -k IDLE     -I 1 -s 0
+hashpipe_check_status -k WEBCNTRL -I 0 -s 1
+hashpipe_check_status -k WEBCNTRL -I 1 -s 1
