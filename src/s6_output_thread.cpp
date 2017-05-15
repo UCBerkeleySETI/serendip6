14b62256c4780937d180e3675fd3f4014e8291c5
Jeff Cobb <jeffc@ssl.berkeley.edu>
2017-05-15 10:52:01 -0700

    Remove some old commented out code.
    This code was once used for:
    - tracking missed packets (tracked elsewhere now)
    - debugging printf()'s

--- src/s6_output_thread.cpp
+++ src/s6_output_thread.cpp
@@ -263,27 +263,6 @@ static void *run(hashpipe_thread_args_t * args)
                   run_always      != prior_run_always  ||
                   num_coarse_chan != db->block[block_idx].header.num_coarse_chan) {
 
-#if 0
-//            uint32_t total_missed_pkts[N_BEAM_SLOTS];
-//
-//            char missed_key[9] = "MISSPKBX";
-//            const char missed_beam[7] = {'0', '1', '2', '3', '4', '5', '6'};
-//            for(int i=0; i < N_BEAMS; i++) {
-//                missed_key[7] = missed_beam[i];
-//                hgetu4(st.buf, missed_key, &total_missed_pkts[i]);
-//                hputu4(st.buf, missed_key, 0);
-//            }
-//
-//            hashpipe_info(__FUNCTION__, "Missed packet totals : beam0 %lu beam1 %lu beam2 %lu beam3 %lu beam4 %lu beam5 %lu beam6 %lu \n",
-//                         total_missed_pkts[0], total_missed_pkts[1], total_missed_pkts[2], total_missed_pkts[3], 
-//                         total_missed_pkts[4], total_missed_pkts[5], total_missed_pkts[6]);
-#endif
-
-//fprintf(stderr, "prior receiver %s (%ld)  status receiver %s (%ld)\n", prior_receiver, strlen(prior_receiver),gbtstatus.IFV1TNCI, strlen(gbtstatus.IFV1TNCI));
-//fprintf(stderr, "prior run_always %ld  run_always %ld\n", prior_run_always, run_always);
-//fprintf(stderr, "prior num_coarse_chan %ld  num_coarse_chan %ld\n", num_coarse_chan, db->block[block_idx].header.num_coarse_chan);
-//fprintf(stderr, "prior webcntrl %ld\n", gbtstatus.WEBCNTRL);
-
             hashpipe_info(__FUNCTION__, "Initializing output for %ld coarse channels, using receiver %s\n",
 #ifdef SOURCE_S6
                           num_coarse_chan, receiver[scram.receiver]);
